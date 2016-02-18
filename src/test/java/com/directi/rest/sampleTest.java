92cd2e9d8573d6de8e82526de91efb13210528ce
sarvendra <sarvendraanand@gmail.com>
2016-02-18 15:30:16 +0530

    Add sample test case

---
+++ src/test/java/com/directi/rest/sampleTest.java
@@ -0,0 +1,17 @@
+package com.directi.rest;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by sarvendraanand on 2/18/16.
+ */
+public class sampleTest {
+
+    @Test
+    public void validate() {
+        assertEquals(true, false);
+    }
+
+}
